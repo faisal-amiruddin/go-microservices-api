5323b6bc26a106225db6bd41b396f44c3d6473e9
Faisal Amiruddin <faisalamrdn@gmail.com>
2026-01-31 09:16:37 +0700

    Router Basics/XML Encoding

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name	string	`json:"full_name"`
-	City	string	`json:"city"`
-	Zipcode	string	`json:"zip_code"`
+	Name	string	`json:"full_name"	xml:"name"`
+	City	string	`json:"city"		xml:"city"`
+	Zipcode	string	`json:"zip_code"	xml:"zipcode"`
 }
 
 func main() {
@@ -32,7 +33,13 @@ func getAllCustomer(w http.ResponseWriter, r *http.Request) {
 		{ "Sasuke", "konoha", "12345" },
 	}
 
-	w.Header().Add("Content-Type", "application/json")
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 
-	json.NewEncoder(w).Encode(customers)
+
 }
